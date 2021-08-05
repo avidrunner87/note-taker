82621e49f73998f2d39fa9f125002fe59a3e7e04
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-08-04 23:13:40 -0500

    Built backend and frontend with three API calls

---
+++ public/assets/scripts/script.js
@@ -0,0 +1,183 @@
+let noteTitle;
+let noteText;
+let saveNoteBtn;
+let newNoteBtn;
+let noteList;
+
+if (window.location.pathname === '/notes') {
+    noteTitle = document.querySelector('#noteTitle');
+    noteText = document.querySelector('#noteDescription');
+    saveNoteBtn = document.querySelector('.save-note');
+    newNoteBtn = document.querySelector('.new-note');
+    noteList = document.querySelectorAll('.list-container .list-group');
+}
+
+// activeNote is used to keep track of the note in the textarea
+let activeNote = {};
+
+const getNotes = () =>
+    fetch('/api/notes', {
+        method: 'GET',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+    });
+
+const saveNote = (note) =>
+    fetch('/api/notes', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(note),
+    });
+
+const deleteNote = (id) =>
+    fetch(`/api/notes/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+// Render the list of note titles
+const renderNoteList = async (notes) => {
+    let jsonNotes = await notes.json();
+    if (window.location.pathname === '/notes') {
+        noteList.forEach((el) => (el.innerHTML = ''));
+    }
+
+    let noteListItems = [];
+
+    // Returns HTML element with or without a delete button
+    const createLi = (text, delBtn = true) => {
+        const liEl = document.createElement('li');
+
+        const divColEl = document.createElement('div');
+        divColEl.classList.add('col', 's12');
+
+        const divCardEl = document.createElement('div');
+        divCardEl.classList.add('card', 'blue-grey', 'lighten-5', 'black-text');
+
+        const divContentEl = document.createElement('div');
+        divContentEl.classList.add('card-content');
+
+        const spanEl = document.createElement('span');
+        spanEl.classList.add('card-title');
+        spanEl.innerText = text;
+
+        if (delBtn) {
+            spanEl.addEventListener('click', handleNoteView);
+            const aEl = document.createElement('a');
+            aEl.classList.add('btn-floating', 'halfway-fab', 'btn-flat', 'blue-grey', 'lighten-5', 'black-text', 'right', 'delete-note');
+            aEl.addEventListener('click', handleNoteDelete);
+
+            const iEl = document.createElement('i');
+            iEl.classList.add('material-icons', 'black-text');
+            iEl.innerText = 'clear';
+
+            aEl.append(iEl);
+            spanEl.append(aEl);
+        }
+
+        divContentEl.append(spanEl);
+        divCardEl.append(divContentEl);
+        divColEl.append(divCardEl);
+        liEl.append(divColEl);
+
+        return liEl;
+    };
+
+    if (jsonNotes.length === 0) {
+        noteListItems.push(createLi('No saved Notes', false));
+    }
+
+    jsonNotes.forEach((note) => {
+        const li = createLi(note.title);
+        li.dataset.note = JSON.stringify(note);
+
+        noteListItems.push(li);
+    });
+
+    if (window.location.pathname === '/notes') {
+        noteListItems.forEach((note) => noteList[0].append(note));
+    }
+};
+
+// Sets the activeNote and displays it
+const handleNoteView = (e) => {
+    e.preventDefault();
+    activeNote = JSON.parse(e.target.closest('li').getAttribute('data-note'));
+    renderActiveNote();
+};
+
+const handleNewNoteView = (e) => {
+    activeNote = {};
+    renderActiveNote();
+};
+
+const renderActiveNote = () => {
+    if (activeNote.id) {
+        document.getElementById('note-card-title').innerText = 'Update Note';
+        noteTitle.value = activeNote.title;
+        noteID.value = activeNote.id;
+        noteDescription.value = activeNote.text;
+    } else {
+        document.getElementById('note-card-title').innerText = 'Add Note';
+        noteTitle.value = '';
+        noteID.value = '';
+        noteDescription.value = '';
+    }
+
+    handleRenderSaveBtn();
+};
+
+const handleNoteSave = () => {
+    const newNote = {
+        id: noteID.value || Date.now(),
+        title: noteTitle.value,
+        text: noteText.value,
+    };
+    saveNote(newNote).then(() => {
+        activeNote = {};
+        getAndRenderNotes();
+        renderActiveNote();
+    });
+};
+
+const handleNoteDelete = (e) => {
+    // Prevents the click listener for the list from being called when the button inside of it is clicked
+    e.stopPropagation();
+
+    const note = e.target;
+    const noteId = JSON.parse(note.closest('li').getAttribute('data-note')).id;
+
+    if (activeNote.id === noteId) {
+        activeNote = {};
+    }
+
+    deleteNote(noteId).then(() => {
+        getAndRenderNotes();
+        renderActiveNote();
+    });
+};
+
+const handleRenderSaveBtn = () => {
+    if (!noteTitle.value.trim() || !noteText.value.trim()) {
+        saveNoteBtn.classList.add('disabled');
+    } else {
+        saveNoteBtn.classList.remove('disabled');
+    }
+};
+
+// Gets notes from the db and renders them to the sidebar
+const getAndRenderNotes = () => getNotes().then(renderNoteList);
+
+if (window.location.pathname === '/notes') {
+    saveNoteBtn.addEventListener('click', handleNoteSave);
+    newNoteBtn.addEventListener('click', handleNewNoteView);
+    noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+    noteText.addEventListener('keyup', handleRenderSaveBtn);
+}
+
+getAndRenderNotes();
---
+++ server.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.port || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Add a static middleware for serving assets in the public folder
+app.use(express.static('public'));
+
+// Return index.html page
+app.get('/', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/'))
+);
+
+// Return notes.html page
+app.get('/notes', (req, res) => 
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
+
+// Return JSON list of all notes notes in the db.json file
+app.get('/api/notes', (req, res) => {
+    // Log our request to the terminal
+    console.log('----------');
+    console.log('Time:', Date(Date.now()).toString());
+    console.log('Method:', req.method);
+    console.log('URL Path:', req.originalUrl);
+    console.log('Description:', 'Request for all notes in database');
+    // Obtain existing notes
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            let parsedNotes = JSON.parse(data);
+            console.log(parsedNotes);
+            res.json(parsedNotes);
+            return;
+        }
+    });
+});
+
+// Create a new note in the db.json file
+app.post('/api/notes', (req, res) => {
+    const noteId = req.body.id;
+
+    // Log our request to the terminal
+    console.log('----------');
+    console.log('Time:', Date(Date.now()).toString());
+    console.log('Method:', req.method);
+    console.log('URL Path:', req.originalUrl);
+    console.log('Description:', 'Request for all notes in database');
+    console.log('Body', req.body);
+
+    // Obtain existing notes
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            let parsedNotes = JSON.parse(data);
+
+            // Remove the deleted note from the database
+            const noteIndex = parsedNotes.findIndex(note => (note.id == noteId));
+            if (noteIndex !== -1) {
+                parsedNotes.splice(noteIndex, 1);
+            }
+
+            parsedNotes.push(req.body);
+            console.log(parsedNotes);
+
+            // Write updated notes back to the file
+            fs.writeFile(
+                './db/notes.json',
+                JSON.stringify(parsedNotes),
+                (writeErr) => {
+                    writeErr
+                    ? console.error(writeErr)
+                    : console.log('Successfully updated notes!');
+                    res.json(`Note with ID ${noteId} has been successfully updated`);
+                    return;
+                }
+            );
+        }
+    });
+
+});
+
+// Delete a note in the db.json file
+app.delete('/api/notes/:id', (req, res) => {
+    const noteId = req.params.id;
+
+    // Log our request to the terminal
+    console.log('----------');
+    console.log('Time:', Date(Date.now()).toString());
+    console.log('Method:', req.method);
+    console.log('URL Path:', req.originalUrl);
+    console.log('Description:', `Delete request received for note with ID ${req.params.id}`);
+
+    // Obtain existing notes
+    fs.readFile('./db/notes.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+        } else {
+            // Convert string into JSON object
+            let parsedNotes = JSON.parse(data);
+
+            // Remove the deleted note from the database
+            const noteIndex = parsedNotes.findIndex(note => (note.id == noteId));
+            console.log(noteIndex);
+            if (noteIndex !== -1) {
+                parsedNotes.splice(noteIndex, 1);
+            }
+
+            console.log(parsedNotes);
+
+            // Write updated notes back to the file
+            fs.writeFile(
+                './db/notes.json',
+                JSON.stringify(parsedNotes),
+                (writeErr) => {
+                    writeErr
+                    ? console.error(writeErr)
+                    : console.log('Successfully updated notes!');
+                    res.json(`Note with ID ${noteId} has been successfully deleted`);
+                    return;
+                }
+            );
+        }
+    });
+});
+
+app.listen(PORT, () =>
+    console.log(
+    `Launch the note taker app at http://localhost:${PORT}`
+    )
+);
